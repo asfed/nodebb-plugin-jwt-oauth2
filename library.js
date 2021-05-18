d59bfbe3ac267b0aac166feea447f704184e16b1
Andrey Fedorov <asfedorov@gmail.com>
2021-05-18 11:22:12 +0300

    Update library.js

--- library.js
+++ library.js
@@ -80,11 +80,14 @@
                 opts.callbackURL = nconf.get('url') + '/auth/' + constants.name + '/callback';
 
                 passportOAuth.Strategy.prototype.userProfile = function(accessToken, done) {
-                    const uid = OAuth.getUidFromToken(accessToken);
-                    this._oauth2.get(constants.userRoute + uid, accessToken, function(err, body, res) {
+                    //const uid = OAuth.getUidFromToken(accessToken);
+                    //this._oauth2.get(constants.userRoute + uid, accessToken, function(err, body, res) {
+                    this._oauth2.get(constants.userRoute, accessToken, function(err, body, res) {
                         if (err) {
                             return done(new InternalOAuthError('failed to fetch user profile', err));
                         }
+                        
+                        console.log(body);
 
                         try {
                             var json = JSON.parse(body);
